80f898f2b0725f0d6117098f58d33c6855547be1
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2020-11-23 17:02:59 +0100

    Second commit
    Suppression des fonts non utilisées
    Ajout d'un projet : showtrack
    Résolution du bug de la section diagonale

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 today = new Date()
-if (today.getHours() >= 0 && today.getHours() < 18) /* A noter que si on mettait <= Bonjour continuerait jusqu'à 18h59 et qu'ici il s'arrête à 18h */ {
+if (today.getHours() >= 0 && today.getHours() < 18)
+{
     document.getElementById('bonjour').innerHTML = 'Bonjour';
-} else {
+} 
+else 
+{
     document.getElementById('bonjour').innerHTML = 'Bonsoir';
 }
