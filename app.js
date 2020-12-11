ad5002f20b6ec132a271b39e514e04971a0c4f18
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2020-12-11 11:00:50 +0100

    Modification du script qui modifie mon titre h1 en fonction de l'heure.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-today = new Date()
-if (today.getHours() >= 0 && today.getHours() < 18)
-{
-    document.getElementById('bonjour').innerHTML = 'Bonjour';
-} 
-else 
-{
-    document.getElementById('bonjour').innerHTML = 'Bonsoir';
+'use strict';
+
+function updateHello() {
+    const today = new Date();
+    const hello = document.getElementById('bonjour');
+    if (today.getHours() >= 8 && today.getHours() < 18) {
+        hello.innerHTML = 'Bonjour';
+    } else {
+        hello.innerHTML = 'Bonsoir';
+    }
 }
+updateHello();
